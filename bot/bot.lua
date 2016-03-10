dc9e170b8ff76e14b1cd8fe5bb6f0137ba7dbf23
Shayan123456 <shayan_jon123@yahoo.com>
2016-03-10 14:24:35 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -264,7 +264,7 @@ function create_config( )
       "webshot",
       "arabic"
      },
-    sudo_users = {87160007,172178919,41471906},
+    sudo_users = {87160007,172178919},
     admin_users = {},
     disabled_channels = {}
   }
